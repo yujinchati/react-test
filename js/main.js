decd8f5ea855bc2db4727bb9b40ba7a13e4f9b5e
yujin <yujin.chati@gmail.com>
2023-11-13 21:34:25 +0900

    미션3 함수분리

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-/* 클릭 이벤트 */
 const box = document.querySelector('.box');
 const btns = document.querySelectorAll('li');
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', (e) => {
 		btns.forEach((btn) => btn.classList.remove('on'));
-		btns[idx].classList.add('on');
-
-		//배경색 변경 이벤트 추가
+		btnClickOn(btns[idx]);
 		const backColor = btns[idx].innerText;
-		box.style.backgroundColor = backColor;
+		boxColor(backColor);
 	});
 });
+
+//버튼 활성화 함수
+function btnClickOn(e) {
+	e.classList.add('on');
+}
+
+//배경색 변경 함수
+function boxColor(color) {
+	box.style.backgroundColor = color;
+}
