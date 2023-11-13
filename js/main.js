b86e1d67bb183dd81b609d1aa4d9c381cc7960cd
yujin <yujin.chati@gmail.com>
2023-11-13 21:18:03 +0900

    미션2 배경색 변경 추가

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 /* 클릭 이벤트 */
+const box = document.querySelector('.box');
 const btns = document.querySelectorAll('li');
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', (e) => {
 		btns.forEach((btn) => btn.classList.remove('on'));
 		btns[idx].classList.add('on');
+
+		//배경색 변경 이벤트 추가
+		const backColor = btns[idx].innerText;
+		box.style.backgroundColor = backColor;
 	});
 });
